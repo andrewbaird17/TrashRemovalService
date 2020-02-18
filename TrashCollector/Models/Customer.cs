bec3f3869f5fdcf85850d97a514ed9dbb68bd9c3
Andrew Baird <baird.andrew4@gmail.com>
2020-02-18 14:41:26 -0600

    Deleted ApplicationUser Model. Added ForeignKey of IdentityUser to Customer and Employee and remigrated

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -26,6 +27,10 @@ namespace TrashCollector.Models
         [ForeignKey("PickUp")]
         public int PickUpId { get; set; }
         public PickUp PickUp { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser Identityuser { get; set; }
        
     }
 }
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -13,6 +14,10 @@ namespace TrashCollector.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int RouteZipCode { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser Identityuser { get; set; }
 
     }
 }
