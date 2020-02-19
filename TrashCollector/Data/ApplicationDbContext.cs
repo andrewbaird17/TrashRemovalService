d387eb16cfeecc9bc4d668390f7490437cd89a33
Andrew Baird <baird.andrew4@gmail.com>
2020-02-19 11:47:37 -0600

    Started to lay down some ground work for seeding database for kthe future

--- TrashCollector/Data/ApplicationDbContext.cs
+++ TrashCollector/Data/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ namespace TrashCollector.Data
             : base(options)
         {
         }
-        
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Address> Adresses { get; set; }
@@ -28,12 +28,83 @@ namespace TrashCollector.Data
                     {
                         Name = "Employee",
                         NormalizedName = "EMPLOYEE"
-                    }, new IdentityRole
+                    },
+                    new IdentityRole
                     {
                         Name = "Customer",
                         NormalizedName = "CUSTOMER"
                     }
                 );
+            //base.OnModelCreating(builder);
+            //builder.Entity<Employee>()
+            //    .HasData(
+            //        new Employee
+            //        {
+            //            Id = 1,
+            //            Name = "Steve",
+            //            RouteZipCode = 53217
+
+            //        },
+            //        new Employee
+            //        {
+            //            Id = 2,
+            //            Name = "Alex",
+            //            RouteZipCode = 53151
+            //        }
+            //    );
+            //base.OnModelCreating(builder);
+            //builder.Entity<Address>()
+            //    .HasData(
+            //        new Address
+            //        {
+            //            Id = 1,
+            //            StreetAddress = "12821 W Ohio Dr",
+            //            City = "New Berlin",
+            //            State = "Wisconsin",
+            //            ZipCode = 53151
+
+            //        },
+            //        new Address
+            //        {
+            //            Id = 2,
+            //            StreetAddress = "15636 W El Dorado Dr",
+            //            City = "New Berlin",
+            //            State = "Wisconsin",
+            //            ZipCode = 53151
+            //        },
+            //        new Address
+            //        {
+            //            Id = 3,
+            //            StreetAddress = "5723 N Santa Monica Blvd",
+            //            City = "Whitefish Bay",
+            //            State = "Wisconsin",
+            //            ZipCode = 53217
+            //        },
+            //        new Address
+            //        {
+            //            Id = 4,
+            //            StreetAddress = "1020 E Quarles Pl",
+            //            City = "Fox Point",
+            //            State = "Wisconsin",
+            //            ZipCode = 53217
+            //        },
+            //        new Address
+            //        {
+            //            Id = 5,
+            //            StreetAddress = "8112 N Poplar Dr",
+            //            City = "Fox Point",
+            //            State = "Wisconsin",
+            //            ZipCode = 53217
+            //        },
+            //        new Address
+            //        {
+            //            Id = 6,
+            //            StreetAddress = "13295 W Old Oak Ln",
+            //            City = "New Berlin",
+            //            State = "Wisconsin",
+            //            ZipCode = 53151
+            //        }
+            //    );
         }
     }
 }
