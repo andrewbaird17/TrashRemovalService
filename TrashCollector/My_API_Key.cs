a420aedf934cf57bbc7d64084c1912e622ac5f36
Andrew Baird <baird.andrew4@gmail.com>
2020-02-25 07:46:08 -0600

    Have a marker showing up on the map

---
+++ TrashCollector/My_API_Key.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector
+{
+    public static class My_API_Key
+    {
+        private static readonly string key = "AIzaSyDBVhKnqThTyhe8_HxLPNRQPDnfEKTmzwQ";
+        public static string Key
+        {
+            get
+            {
+                return key;
+            }
+        }
+    }
+}
